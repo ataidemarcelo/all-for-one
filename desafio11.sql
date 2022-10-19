d341a6116302a4e382552bf800cf3ccc826193e1
Marcelo Ataíde <msa.trybe@gmail.com>
2022-10-19 16:34:19 -0300

    Requisito 11 - Exiba os dados da coluna notes da tabela purchase_orders em que seu valor de...

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT notes FROM northwind.purchase_orders WHERE notes IN (
+	'Purchase generated based on Order #30', 
+    'Purchase generated based on Order #31',
+    'Purchase generated based on Order #32',
+    'Purchase generated based on Order #33',
+    'Purchase generated based on Order #34',
+    'Purchase generated based on Order #35',
+    'Purchase generated based on Order #36',
+    'Purchase generated based on Order #37',
+    'Purchase generated based on Order #38',
+    'Purchase generated based on Order #39'
+);
